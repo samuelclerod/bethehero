d603631703979dd1f430b81af4d8937e1e55cc77
Samuel Rodrigues <samuelclerod@gmail.com>
2020-03-27 10:02:07 -0300

    tela de login e cadastrado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react'
+
+import './global.css'
+
+import Routes from './routes'
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+    <Routes />
   );
 }
 
-export default App;
+export default App
